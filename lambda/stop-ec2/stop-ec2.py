d9991f45c1a899d5dada882d157ebf25e9ec7ce8
Vitor Mateus <vitor7jan@gmail.com>
2022-11-21 21:09:22 -0300

    chore(config): lambda stop ec2 python code

---
+++ lambda/stop-ec2/stop-ec2.py
@@ -0,0 +1,7 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    filters = [{'Name': 'tag-key', 'Values': ['LambdaTurnOff']}]
+    ec2.instances.filter(Filters=filters).stop()
