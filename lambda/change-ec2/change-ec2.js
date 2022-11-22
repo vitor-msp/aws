dc745298931405793de103cf6edf56f32fbc0c23
Vitor Mateus <vitor7jan@gmail.com>
2022-11-21 21:51:07 -0300

    chore(config): lambda change ec2 nodejs code

---
+++ lambda/change-ec2/change-ec2.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+
+exports.handler = (event, context, callback) => {
+  const { instanceId, instanceRegion, instanceType } = event;
+
+  const ec2 = new AWS.EC2({ region: instanceRegion });
+
+  Promise.resolve()
+    .then(() => ec2.stopInstances({ InstanceIds: [instanceId] }).promise())
+    .then(() =>
+      ec2.waitFor("instanceStopped", { InstanceIds: [instanceId] }).promise()
+    )
+    .then(() =>
+      ec2
+        .modifyInstanceAttribute({
+          InstanceId: instanceId,
+          InstanceType: { Value: instanceType },
+        })
+        .promise()
+    )
+    .then(() => ec2.startInstances({ InstanceIds: [instanceId] }).promise())
+    .then(() =>
+      callback(
+        null,
+        `Successfully modified ${instanceId} to ${instanceType}!`
+      )
+    )
+    .catch((err) => callback(err));
+};
